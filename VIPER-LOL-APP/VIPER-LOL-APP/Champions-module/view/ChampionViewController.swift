46ac530e8d54e17db70085f1b222e2a8380947bd
edualb <eduardoslafiq@gmail.com>
2019-08-14 13:49:39 -0300

    Activity Indicator Added

--- VIPER-LOL-APP/VIPER-LOL-APP/Champions-module/view/ChampionViewController.swift
+++ VIPER-LOL-APP/VIPER-LOL-APP/Champions-module/view/ChampionViewController.swift
@@ -13,9 +13,12 @@ final class ChampionViewController: UITableViewController {
     
     var championsRotation: [ChampionModelView]?
     var presenter: ChampionWeekPresenterProtocol?
+    var activityIndicator = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black
+        self.beginActivityIndicator(view: self.tableView)
         presenter?.viewDidLoad()
     }
 
@@ -27,7 +30,6 @@ final class ChampionViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "championsCell")
         let customBackgroundColor = UIView()
         customBackgroundColor.backgroundColor = UIColor.black
-        
         cell?.textLabel?.text = championsRotation?[indexPath.row].name
         cell?.textLabel?.textColor = UIColor.white
         cell?.selectedBackgroundView = customBackgroundColor
@@ -45,6 +47,20 @@ extension ChampionViewController: ChampionWeekPresenterDelegate {
     func showWeekChampions(with champions: [ChampionModelView]) {
         self.championsRotation = champions
         self.tableView.reloadData()
+        self.stopActivityIndicator()
     }
     
+    private func beginActivityIndicator(view: UIView) {
+        self.activityIndicator.center = view.center
+        self.activityIndicator.hidesWhenStopped = true
+        self.activityIndicator.style = UIActivityIndicatorView.Style.white
+        view.addSubview(activityIndicator)
+        self.activityIndicator.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
+    }
+    
+    private func stopActivityIndicator() {
+        self.activityIndicator.stopAnimating()
+        UIApplication.shared.endIgnoringInteractionEvents()
+    }
 }
