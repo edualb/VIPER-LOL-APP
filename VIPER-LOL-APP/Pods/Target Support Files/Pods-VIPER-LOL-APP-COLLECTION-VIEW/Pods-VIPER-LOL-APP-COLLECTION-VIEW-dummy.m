1bfbfc1307d9c7cb91bb53c78cc811210d231ead
edualb <eduardoslafiq@gmail.com>
2019-08-22 15:48:09 -0300

    Collection view project added

---
+++ VIPER-LOL-APP/Pods/Target Support Files/Pods-VIPER-LOL-APP-COLLECTION-VIEW/Pods-VIPER-LOL-APP-COLLECTION-VIEW-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VIPER_LOL_APP_COLLECTION_VIEW : NSObject
+@end
+@implementation PodsDummy_Pods_VIPER_LOL_APP_COLLECTION_VIEW
+@end
---
+++ VIPER-LOL-APP/VIPER-LOL-APP-COLLECTION-VIEW/VIPER-LOL-APP-COLLECTION-VIEW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER-LOL-APP-COLLECTION-VIEW
+//
+//  Created by eduardo.silva on 22/08/19.
+//  Copyright © 2019 eduardo.silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
