c55449750b5f0ed67746fec77a1b91e30c57b7a2
edualb <eduardoslafiq@gmail.com>
2019-08-26 12:56:32 -0300

    test includes in pod file

---
+++ VIPER-LOL-APP/Pods/Target Support Files/Pods-VIPER-LOL-APP-COLLECTION-VIEWTests/Pods-VIPER-LOL-APP-COLLECTION-VIEWTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VIPER_LOL_APP_COLLECTION_VIEWTests : NSObject
+@end
+@implementation PodsDummy_Pods_VIPER_LOL_APP_COLLECTION_VIEWTests
+@end
---
+++ VIPER-LOL-APP/Pods/Target Support Files/Pods-VIPER-LOL-APP-COLLECTION-VIEWUITests/Pods-VIPER-LOL-APP-COLLECTION-VIEWUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VIPER_LOL_APP_COLLECTION_VIEWUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VIPER_LOL_APP_COLLECTION_VIEWUITests
+@end
