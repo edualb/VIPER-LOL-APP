596e2ae5804c8b43c3786ef1c5c70501e27df09e
edualb <eduardoslafiq@gmail.com>
2019-08-29 15:06:25 -0300

    TableView on tvOS

--- VIPER-LOL-APP/VIPER-LOL-APP-TVOS/VIPER-LOL-APP-TVOS/championModule/view/ChampionTvViewController.swift
+++ VIPER-LOL-APP/VIPER-LOL-APP-TVOS/VIPER-LOL-APP-TVOS/championModule/view/ChampionTvViewController.swift
@@ -8,19 +8,41 @@
 
 import UIKit
 
-class ChampionTvViewController: UIViewController {
+final class ChampionTvViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     private var presenter: ChampionPresenterProtocol?
+    private var champions: [ChampionModelView]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.backgroundColor = .black
+        self.view.backgroundColor = .black
+        presenter?.viewDidLoad()
     }
 
 }
 
+extension ChampionTvViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return champions?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "championsCell", for: indexPath) as! ChampionTableViewCell
+        cell.setupCell(with: champions![indexPath.row])
+        return cell
+    }
+    
+}
+
 extension ChampionTvViewController: ChampionWeekPresenterDelegate {
     func showWeekChampions(with champions: [ChampionModelView]) {
-        
+        self.champions = champions
+        self.tableView.reloadData()
     }
     
     func showMsgError(with msg: String) {
--- VIPER-LOL-APP/VIPER-LOL-APP/VIPER-LOL-APP/ChampionsModule/view/ChampionViewController.swift
+++ VIPER-LOL-APP/VIPER-LOL-APP/VIPER-LOL-APP/ChampionsModule/view/ChampionViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import LeagueAPI
 
 final class ChampionViewController: UIViewController {
     
